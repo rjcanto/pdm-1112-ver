b4c405f486df565a4115ee87cc96fb2c7d474d1c
luis.bras@gmail.com <luis.bras@gmail.com@6ffb6f18-76ca-7abc-35ff-248637178ceb>
2012-05-30 17:36:18 +0000

    LB Database

---
+++ trunk/src/pt/isel/pdm/Yamba/TimelineContract.java
@@ -0,0 +1,16 @@
+package pt.isel.pdm.Yamba;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class TimelineContract {
+	public static final String TABLE 	= "UC";
+	//public static final Uri CONTENT_URI = Uri.withAppendedPath(LeicProvider.CONTENT_URI, TABLE);
+	
+	public static final String 
+	  _ID 			= BaseColumns._ID,
+  	  CREATED_AT 	= "createdAt",
+	  TEXT 			= "text",
+	  IS_READ	 	= "isRead",
+	  AUTHOR_ID		= "authorId";
+}
--- trunk/src/pt/isel/pdm/Yamba/Utils.java
+++ trunk/src/pt/isel/pdm/Yamba/Utils.java
@@ -2,12 +2,18 @@ package pt.isel.pdm.Yamba;
 
 import android.content.Context;
 import android.widget.Toast;
+import android.util.Log;
 
 public final class Utils {
 
 	/** Displays a Toast with long length duration */ 
 	public static void showToast(Context ctx, String txt) {
 		Toast.makeText(ctx, txt, Toast.LENGTH_LONG).show();
+	}
+	
+	private static final String TAG = "PDM";
+	public static void Log(String txt) {
+		Log.d(TAG, txt);
 	}
 	
 }
